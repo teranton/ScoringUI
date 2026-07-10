ed6af19f8c2821af964cfe1f6406cfeb92fe0144
Tero Rantonen <tero.rantonen@elektrobit.com>
2026-07-10 09:10:10 +0300

    Button transition all -> transition colors

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -4,7 +4,7 @@ import { cva } from 'class-variance-authority';
 import { cn } from '../../lib/utils';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-[hsl(var(--ring))] ring-offset-[hsl(var(--background))]',
+  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors disabled:pointer-events-none disabled:opacity-50 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-[hsl(var(--ring))] ring-offset-[hsl(var(--background))]',
   {
     variants: {
       variant: {
